a2015a3154764c0d59a5fc048c13fac993cd1404
Daniel Amadi <damadiSC121@gmail.com>
2019-03-16 13:13:54 -0400

    Add scaffolds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_16_153715) do
+ActiveRecord::Schema.define(version: 2019_03_16_171306) do
+
+  create_table "albums", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "date"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_albums_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
